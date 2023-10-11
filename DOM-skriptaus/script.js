8dd5e6da0565873f7f29afe19dca164189173715
nellin22 <112803065+nellin22@users.noreply.github.com>
2023-10-11 13:42:28 +0300

    Update script.js

--- DOM-skriptaus/script.js
+++ DOM-skriptaus/script.js
@@ -10,7 +10,7 @@ function newElement() {
   var t = document.createTextNode(inputValue);
   li.appendChild(t);
   if (inputValue === '') {
-    alert("You must write something!");
+    alert("Kirjoita jotain kenttään.");
   } else {
     document.getElementById("myUL").appendChild(li);
   }
