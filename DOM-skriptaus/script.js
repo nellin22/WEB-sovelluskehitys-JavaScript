5c09252cea7fc13fa2c4c7d3fa850e6d7a5d752e
nellin22 <112803065+nellin22@users.noreply.github.com>
2023-10-12 15:30:54 +0300

    Update script.js

--- DOM-skriptaus/script.js
+++ DOM-skriptaus/script.js
@@ -1,56 +1,60 @@
-    var todos = [];
+var todos = [];
 
-    function newElement(event) {
-        event.preventDefault();
-        var li = document.createElement("li");
-        var inputValue = document.getElementById("input").value;
-        if (inputValue === '') {
-            alert("Kirjoita jotain syöttökenttään.");
-            document.getElementById("input").style.borderColor = "red";
-        } else if (inputValue.length < 3) {
-            alert("Sisältö on liian lyhyt.");
-            document.getElementById("input").style.borderColor = "red";
-        } else {
-            li.appendChild(document.createTextNode(inputValue));
-            document.getElementById("list").appendChild(li);
-            todos.push(inputValue);
-            saveToLocalStorage();
+// Kenttään syötetyt tiedot kootaan 'Lisää' nappia painamalla sivulla yhteen listaksi. Virheilmoitukset tyhjästä syöttö kentästä ja liian lyhyestä sisällöstä.
+function newElement(event) {
+    event.preventDefault();
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("input").value;
+    if (inputValue === '') {
+        alert("Kirjoita jotain syöttökenttään.");
+        document.getElementById("input").style.borderColor = "red";
+    } else if (inputValue.length < 3) {
+        alert("Sisältö on liian lyhyt.");
+        document.getElementById("input").style.borderColor = "red";
+    } else {
+        li.appendChild(document.createTextNode(inputValue));
+        document.getElementById("list").appendChild(li);
+        todos.push(inputValue);
+        saveToLocalStorage();
 
-            var span = document.createElement("SPAN");
-            var txt = document.createTextNode("\u00D7");
-            span.className = "close";
-            span.appendChild(txt);
-            li.appendChild(span);
-
-            span.onclick = function() {
-                var div = this.parentElement;
-                div.style.display = "none";
-            };
-        }
-        document.getElementById("input").value = "";
-    }
-
-    var list = document.querySelector('ul');
-    list.addEventListener('click', function(ev) {
-        if (ev.target.tagName === 'LI') {
-            ev.target.classList.toggle('checked');
-        }
-    }, false);
-
-    var myNodelist = document.getElementsByTagName("LI");
-    for (i = 0; i < myNodelist.length; i++) {
         var span = document.createElement("SPAN");
         var txt = document.createTextNode("\u00D7");
         span.className = "close";
         span.appendChild(txt);
-        myNodelist[i].appendChild(span);
+        li.appendChild(span);
 
         span.onclick = function() {
             var div = this.parentElement;
             div.style.display = "none";
         };
     }
+    document.getElementById("input").value = "";
+}
 
-    function saveToLocalStorage() {
-        // Implement your local storage code here if needed.
+// Klikkaamalla listassa olevaa asiaa lisää check markin asian eteen
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+    if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked');
     }
+}, false);
+
+// Tekee ruksi nappulan jokaisen listassa olevan asian oikeaan laitaan ja mahdollistaa asioiden poistamisen listasta ruksia painamalla
+var myNodelist = document.getElementsByTagName("LI");
+for (i = 0; i < myNodelist.length; i++) {
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    myNodelist[i].appendChild(span);
+
+    span.onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+    };
+}
+
+//Tallentaa tiedot localstorageen
+function saveToLocalStorage() {
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
