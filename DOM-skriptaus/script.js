68b25f92ef0be8b944fdd9505460d622254c85aa
nellin22 <112803065+nellin22@users.noreply.github.com>
2023-10-13 13:18:45 +0300

    Update script.js

--- DOM-skriptaus/script.js
+++ DOM-skriptaus/script.js
@@ -1,3 +1,4 @@
+// Alustaa tyhjän taulukon tietojen tallentamista varten
 var todos = [];
 
 //Tallentaa tiedot localstorageen
@@ -5,52 +6,50 @@ function saveToLocalStorage() {
   localStorage.setItem("todos", JSON.stringify(todos));
 }
 
-// Kenttään syötetyt tiedot kootaan 'Lisää' nappia painamalla sivulla yhteen listaksi. Virheilmoitukset tyhjästä syöttö kentästä ja liian lyhyestä sisällöstä.
 function newElement(event) {
-    event.preventDefault();
+  
+// Luo listan ja hankkii tiedot käyttäjältä
     var li = document.createElement("li");
     var inputValue = document.getElementById("input").value;
-    if (inputValue === '') {
+  
+  // Antaa virheilmoitukset tyhjästä syöttökentästä ja liian lyhyestä sisällöstä. Vaihtaa syöttökentän reunat punaiseksi virheen ilmaantuessa.
+  if (inputValue === '') {
         alert("Kirjoita jotain syöttökenttään.");
         document.getElementById("input").style.borderColor = "red";
     } else if (inputValue.length < 3) {
         alert("Sisältö on liian lyhyt.");
         document.getElementById("input").style.borderColor = "red";
+    
+    // Kenttään syötetyt tiedot kootaan ja liitetään yhdeksi listaksi. Lisää uuden tiedon listan päätteeksi ja tallentaa tiedon localstorageen
     } else {
         li.appendChild(document.createTextNode(inputValue));
         document.getElementById("list").appendChild(li);
         todos.push(inputValue);
         saveToLocalStorage();
 
-// Tekee ruksi nappulan jokaisen listassa olevan asian oikeaan laitaan ja mahdollistaa asioiden poistamisen listasta ruksia painamalla
+    // Luo ruksi -painikkeen jokaisen listassa olevan tietueen oikeaan laitaan.
         var span = document.createElement("SPAN");
         var txt = document.createTextNode("\u00D7");
         span.className = "close";
         span.appendChild(txt);
         li.appendChild(span);
-        
+    
+    // Poistaa tietueen listasta ruksi -painiketta klikkaamalla.   
+    var myNodelist = document.getElementsByTagName("LI");
+    for (i = 0; i < myNodelist.length; i++) {     
         span.onclick = function() {
         var div = this.parentElement;
         div.style.display = "none";
         };
+    // Tyhjentää syöttökentän kun tieto on lisätty listaan Lisää -painiketta klikkaamalla
     }
     document.getElementById("input").value = "";
 }
 
-// Klikkaamalla listassa olevaa asiaa lisää check markin asian eteen
+// Lisää checkmarkin tietueen eteen klikkaamalla
 var list = document.querySelector('ul');
 list.addEventListener('click', function(ev) {
     if (ev.target.tagName === 'LI') {
         ev.target.classList.toggle('checked');
     }
 }, false);
-
-// Tekee ruksi nappulan jokaisen listassa olevan asian oikeaan laitaan ja mahdollistaa asioiden poistamisen listasta ruksia painamalla
-
-var myNodelist = document.getElementsByTagName("LI");
-for (i = 0; i < myNodelist.length; i++) {    
-  span.onclick = function() {
-  var div = this.parentElement;
-  div.style.display = "none";
-  };
-}
