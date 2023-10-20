33d1a4f9c381ac0ebb1858a043a29c6652bcfb17
nellin22 <112803065+nellin22@users.noreply.github.com>
2023-10-20 03:22:53 +0300

    Update script.js

--- DOM-skriptaus/script.js
+++ DOM-skriptaus/script.js
@@ -1,57 +1,54 @@
 // Alustaa tyhjän taulukon tietojen tallentamista varten.
 var todo = [];
 
-// Estää lomakkeen käyttäytymisen oletusmaisesti.
-function newElement(event) {
-  event.preventDefault();
+function newElement(tapaus) {
+  tapaus.preventDefault();
   
 // Luo listan ja hankkii tiedot käyttäjältä.
     var li = document.createElement("li");
-    var inputValue = document.getElementById("input").value;
+    var inputValue = document.getElementById("syöttö").value;
   
   // Antaa virheilmoitukset tyhjästä syöttökentästä ja liian lyhyestä sisällöstä. Vaihtaa syöttökentän reunat punaiseksi virheen ilmaantuessa.
   if (inputValue === '') {
         alert("Kirjoita jotain syöttökenttään.");
-        document.getElementById("input").style.borderColor = "red";
+        document.getElementById("syöttö").style.borderColor = "red";
     } else if (inputValue.length < 3) {
         alert("Sisältö on liian lyhyt.");
-        document.getElementById("input").style.borderColor = "red";
+        document.getElementById("syöttö").style.borderColor = "red";
     
     // Kenttään syötetyt tiedot kootaan ja liitetään yhdeksi listaksi. Uusin tieto lisätään listan perälle.
     } else {
         li.appendChild(document.createTextNode(inputValue));
-        document.getElementById("list").appendChild(li);
+        document.getElementById("lista").appendChild(li);
         todo.push(inputValue);
 
   // Luo ruksi -painikkeen jokaisen listassa olevan tietueen oikeaan laitaan ja poistaa tietueen listasta ruksi -painiketta klikkaamalla. 
-      var span = document.createElement("SPAN");
+      var span = document.createElement("span");
       var txt = document.createTextNode("\u00D7");
-      span.className = "close";
+      span.className = "poista";
       span.appendChild(txt);
-      li.appendChild(span);    
+      li.appendChild(span);
       span.onclick = function() {
-        var div = this.parentElement;
-        div.style.display = "none";
       };
     
   // Liittää klikkaukset poistotoimintoon.
       var myNodelist = document.getElementsByTagName("LI");
       for (i = 0; i < myNodelist.length; i++) {
       span.onclick = function() {
-        var div = this.parentElement; 
+        var div = this.parentElement;
         div.style.display = "none"; 
       };
     }
     
     // Tyhjentää syöttökentän kun tieto on lisätty listaan Lisää -painiketta klikkaamalla.
     }
-    document.getElementById("input").value = "";
+    document.getElementById("syöttö").value = "";
   }
   
-// Lisää checkmarkin tietueen eteen klikkaamalla.
+// Lisää oikein-merkin tietueen eteen klikkaamalla.
 var list = document.querySelector('ul');
 list.addEventListener('click', function(ev) {
     if (ev.target.tagName === 'LI') {
-        ev.target.classList.toggle('check');
+        ev.target.classList.toggle('oikeinMerkki');
     }
 }, false);
