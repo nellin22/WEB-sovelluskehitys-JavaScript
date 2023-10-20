1fcea93b30f809be177597a93518bb77ba648799
nellin22 <112803065+nellin22@users.noreply.github.com>
2023-10-20 04:03:19 +0300

    Update script.js

--- DOM-skriptaus/script.js
+++ DOM-skriptaus/script.js
@@ -1,12 +1,12 @@
 // Alustaa tyhjän taulukon tietojen tallentamista varten.
-function newElement(event) {
+  function newElement(event) {
   event.preventDefault();
   
 // Luo listan ja hankkii tiedot käyttäjältä.
   var li = document.createElement("li");
   var inputValue = document.getElementById("syöttö").value;
   
-  // Antaa virheilmoitukset tyhjästä syöttökentästä ja liian lyhyestä sisällöstä. Vaihtaa syöttökentän reunat punaiseksi virheen ilmaantuessa.
+// Antaa virheilmoitukset tyhjästä syöttökentästä ja liian lyhyestä sisällöstä. Vaihtaa syöttökentän reunat punaiseksi virheen ilmaantuessa.
   if (inputValue === '') {
         alert("Kirjoita jotain syöttökenttään.");
         document.getElementById("syöttö").style.borderColor = "red";
@@ -14,19 +14,19 @@ function newElement(event) {
         alert("Sisältö on liian lyhyt.");
         document.getElementById("syöttö").style.borderColor = "red";
     
-    // Kenttään syötetyt tiedot kootaan ja liitetään yhdeksi listaksi. Uusin tieto lisätään listan perälle.
+// Kenttään syötetyt tiedot kootaan ja liitetään yhdeksi listaksi. Uusin tieto lisätään listan perälle.
     } else {
         li.appendChild(document.createTextNode(inputValue));
         document.getElementById("lista").appendChild(li);
 
-  // Luo ruksi -painikkeen jokaisen listassa olevan tietueen oikeaan laitaan ja poistaa tietueen listasta ruksi -painiketta klikkaamalla. 
+// Luo ruksi -painikkeen jokaisen listassa olevan tietueen oikeaan laitaan.
      var txt = document.createTextNode("\u00D7");
      var span = document.createElement("span");
         span.className = "poista";
         li.appendChild(span);
         span.appendChild(txt);
     
-  // Liittää klikkaukset poistotoimintoon.
+// Poistaa tietueen listasta ruksi -painiketta klikkaamalla.
       var myNodelist = document.getElementsByTagName("LI");
       for (i = 0; i < myNodelist.length; i++) {
       span.onclick = function() {
@@ -36,12 +36,12 @@ function newElement(event) {
     }
   
 // Lisää oikein-merkin tietueen eteen ja yliviivaa sen klikkaamalla.
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-    if (ev.target.tagName === 'LI') {
+  var list = document.querySelector('ul');
+  list.addEventListener('click', function(ev) {
+      if (ev.target.tagName === 'LI') {
         ev.target.classList.toggle('valmis');
-    }
-}, false);
+      }
+  }, false);
     
 // Tyhjentää syöttökentän kun tieto on lisätty listaan Lisää -painiketta klikkaamalla.
   }
